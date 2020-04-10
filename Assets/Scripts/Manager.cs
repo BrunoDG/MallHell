c181b989400d854532744842a1519fad61a1187a
Bruno Dias <brunodg.dev@gmail.com>
2020-04-10 02:10:01 -0300

    Menu Logic working. Singleton file generated for scenes controlling.

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager instance = null; // Declaring and initializing a public static Manager class to null.
+    public string menu = "Main_Menu"; // This is the string that populates the current menu
+
+    void Awake()
+    {
+        if (instance == null) // Determine if our instance is null
+            instance = this; // Assign instance to this instance of the class
+        else if (instance != null) // Determine if our instance is already assigned to something else
+            Destroy(gameObject);
+        
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,14 +1,46 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class StartGame : MonoBehaviour
 {
-    public void LoadScene()
+    
+    public void LoadStartScene()
     {
-        Console.Write("game start!");
+        Manager.instance.menu = "Game";
         SceneManager.LoadScene("Game");
+    }
+
+    public void LoadPreviousScene() 
+    {
+        if (Manager.instance.menu == "Main_Menu") { LoadMainScene(); }
+        else if (Manager.instance.menu == "Options") { LoadOptionsScene(); }
+        else if (Manager.instance.menu == "Game") { LoadStartScene(); }
+    }
+    
+    public void LoadOptionsScene()
+    {
+        Manager.instance.menu = "Options";
+        SceneManager.LoadScene("Options");
+    }
+    
+    public void LoadMainScene()
+    {
+        Manager.instance.menu = "Main_Menu";
+        SceneManager.LoadScene("Main_Menu");
+    }
+
+    public void LoadProfileScene() 
+    {
+        SceneManager.LoadScene("Profile");
+    }
+
+    public void LoadStoreScene() 
+    {
+        SceneManager.LoadScene("Store");
     }
 }
